318105e4870f937fe6f11b29da05ac4ea059d4e5
root <root@DESKTOP-51B99RU.localdomain>
2020-04-30 16:26:52 +0200

    creation de la page d'attente et de la creation de room

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
+var session = require('express-session');
+var bodyParser = require("body-parser");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var roomRouter = require('./routes/room');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('trust proxy', 1) // trust first proxy
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,18 +30,26 @@ app.use(sassMiddleware({
   indentedSyntax: true, // true = .sass and false = .scss
   sourceMap: true
 }));
+app.use(session({
+  secret: "It feels like time is passing so quickly.Damn you, Einstein! Your science is crowding in on our kiss! He was right.The passage of time depends entirely on where you’re standing.Relativity Theory… it’s so romantic.But it’s just so tragic too.",
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/room', roomRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ routes/room.js
@@ -0,0 +1,27 @@
+﻿var express = require('express');
+var router = express.Router();
+
+router.post('/join', function (req, res) {
+  req.session.pseudo = req.body.pseudo
+  res.redirect('/room/' + req.body.roomName)
+});
+
+router.post('/create', function (req, res) {
+  req.session.pseudo = req.body.pseudo
+
+  time = new Date().getTime()
+  code = ""
+  for (let i = 0; i < 8; i++) {
+    code += String.fromCharCode(time % 26 + 65)
+    time = Math.floor(time / 26)
+  }
+
+  res.redirect('/room/' + code.toUpperCase())
+});
+
+/* GET home page. */
+router.get('/:roomName([A-Z]{8})', function (req, res, next) {
+  res.render('room', { title: 'Express', roomName: req.params.roomName });
+});
+
+module.exports = router;
